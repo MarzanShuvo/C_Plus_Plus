bb67e365d996d4bb9da7c9382e190d770c56f5fa
MarzanShuvo <marzanalam3@gmail.com>
2020-10-31 14:56:16 +0600

    pmm_file reader header created

---
+++ Modern C++/assignment2_from_Bonn/igg_image/src/igg_image/io_strategies/ppm_strategy.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<string>
+#include "igg_image/io_strategies/strategy.h"
+
+namespace igg{
+    class PpmIoStrategy : public IoStrategy
+    {
+        public:
+         bool Write(const std::string& file_name, const ImageData& data) const override;
+         ImageData Read(const std::string& file_name) const override;
+    };
+    
+}
